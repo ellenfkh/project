dc887306d1511d4a773c0a61912ec41508b2a8ab
Ellen Hui <ellenfkh@gmail.com>
2014-11-09 23:59:16 -0800

    prelim ir and parser

--- src/main/scala/treeTracer/ir/ir.scala
+++ src/main/scala/treeTracer/ir/ir.scala
@@ -1,1 +1,10 @@
 package treeTracer.ir
+
+sealed abstract class AST
+sealed abstract class Person extends AST
+
+case class Forest(val people:List[Person]) extends Person
+
+case class Node(val name:String) extends Person
+case class Child(val name:String, val parent1:Person, val parent2:Person) extends Person
+
--- src/main/scala/treeTracer/parser/parser.scala
+++ src/main/scala/treeTracer/parser/parser.scala
@@ -1,1 +1,23 @@
 package treeTracer.parser
+
+import scala.util.parsing.combinator._
+import treeTracer.ir._
+
+object PiconotParser extends JavaTokenParsers with PackratParsers {
+  def apply(s:String) :ParseResult[AST] = parseAll(listRules, s)
+
+  lazy val tree:PackratParser[Person] =
+    (
+      rep(person) ^^ {case people => Forest(people)}
+      | failure("failed to parse a Forest")
+    )
+
+  lazy val person:PackratParser[Person] =
+    (
+      | person~"is child of"~person~"and"~person ^^ { case p~"is child of"~p1~"and"~p2 => Child(p, p1, p2)}
+      | """\w+""".r ^^ {Node}
+      | failure("failed to parse a person")
+    )
+
+
+}
