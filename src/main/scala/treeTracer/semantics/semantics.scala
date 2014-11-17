ec733962a0c7563cd380bb20ab8adf16f6bc9b1f
Ellen Hui <ellenfkh@gmail.com>
2014-11-17 01:27:43 -0800

    start semantics

--- src/main/scala/treeTracer/semantics/semantics.scala
+++ src/main/scala/treeTracer/semantics/semantics.scala
@@ -1,1 +1,23 @@
 package treeTracer.semantics
+
+import treeTracer.ir._
+
+package object TreeSemantics {
+  def eval(graph: Graph) = graph match {
+    // FIXME having problems with immutable sets?
+    case Graph(edges) => {
+      var edgeCpy:Set[Edge] = Set.empty[Edge]
+      edges.foreach(x => makeReciprocal(x, edgeCpy))
+      edges = edges ++ edgeCpy
+    }
+  }
+
+  def makeReciprocal(edge: Edge, edgeSet: Set[Edge]) = edge match {
+    case Child(child, parent, "child") => {
+      edgeSet = edgeSet + Parent(parent, child, "parent")
+    }
+    case Parent(parent, child, "child") => {
+      edgeSet = edgeSet + Child(child, parent, "child")
+    }
+  }
+}
